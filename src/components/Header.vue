0a2511a9c0006db3aee82e1df20eb9822232ffc7
渡邊克己 <ktm981124@gmail.com>
2023-05-27 19:40:55 +0900

    表示まで済み

---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <header>
+    <div class="container">
+      <h1><img src="./../assets/logo.svg" alt="logo"></h1>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+}
+</script>
+
+<style scoped></style>
