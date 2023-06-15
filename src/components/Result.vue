2b6b2ab68a966d4e929b05918323ab1591c4fc2e
渡邊克己 <ktm981124@gmail.com>
2023-06-16 00:36:55 +0900

    インデント調整

--- src/components/Result.vue
+++ src/components/Result.vue
@@ -7,78 +7,77 @@
         <router-link to="/" class="cta-button">もう一度診断する！</router-link>
       </div>
     </div>
-  </template>
-  
-  <style scoped>
-  .result-page {
+</template>
+
+<style scoped>
+.result-page {
     display: flex;
     justify-content: center;
     align-items: center;
     height: 50vh;
     background: linear-gradient(to bottom, #41b883, #32a672);
-  }
-  
-  .result-content {
+}
+
+.result-content {
     text-align: center;
-  }
-  
-  .result-title {
+}
+
+.result-title {
     font-size: 48px;
     font-weight: bold;
     color: #ffffff;
     margin-bottom: 20px;
-  }
-  
-  .result-message {
+}
+
+.result-message {
     font-size: 24px;
     font-weight: bold;
     color: #ffffff;
     animation: scaleIn 1s ease-in-out;
-  }
-  
-  .result-description {
+}
+
+.result-description {
     font-size: 18px;
     color: #ffffff;
     margin-top: 10px;
     animation: fadeIn 1s ease-in-out;
     margin-bottom: 30px;
-  }
+}
 
-  .cta-button {
-  padding: 15px 40px;
-  font-size: 18px;
-  font-weight: bold;
-  text-transform: uppercase;
-  background-color: #fff;
-  color: #41b883;
-  border: none;
-  border-radius: 30px;
-  cursor: pointer;
-  transition: background-color 0.3s ease;
+.cta-button {
+    padding: 15px 40px;
+    font-size: 18px;
+    font-weight: bold;
+    text-transform: uppercase;
+    background-color: #fff;
+    color: #41b883;
+    border: none;
+    border-radius: 30px;
+    cursor: pointer;
+    transition: background-color 0.3s ease;
 }
 
 .cta-button:hover {
-  background-color: #ddd;
+    background-color: #ddd;
 }
-  
-  @keyframes scaleIn {
+
+@keyframes scaleIn {
     0% {
-      transform: scale(0);
-      opacity: 0;
+        transform: scale(0);
+        opacity: 0;
     }
     100% {
-      transform: scale(1);
-      opacity: 1;
+        transform: scale(1);
+        opacity: 1;
     }
-  }
-  
-  @keyframes fadeIn {
+}
+
+@keyframes fadeIn {
     0% {
-      opacity: 0;
+        opacity: 0;
     }
     100% {
-      opacity: 1;
+        opacity: 1;
     }
-  }
-  </style>
-  
+}
+</style>
