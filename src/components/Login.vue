c63b78ef97ae7ca92c64e9bcc559e4e7fdea31d7
渡邊克己 <ktm981124@gmail.com>
2023-07-01 22:32:52 +0900

    インデント

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,57 +1,53 @@
 <template>
     <v-app>
-      <v-main>
+        <v-main>
         <v-container fluid class="fill-height">
-          <v-row align="center" justify="center">
+            <v-row align="center" justify="center">
             <v-col cols="12" sm="8" md="4">
-              <v-card>
+                <v-card>
                 <v-card-title class="text-center">
-                  <h2 class="text--primary">Login</h2>
+                    <h2 class="text--primary">Login</h2>
                 </v-card-title>
                 <v-card-text>
-                  <v-form @submit.prevent="login">
+                    <v-form @submit.prevent="login">
                     <v-text-field
-                      v-model="email"
-                      label="Email"
-                      type="email"
-                      required
+                        v-model="email"
+                        label="Email"
+                        type="email"
+                        required
                     ></v-text-field>
                     <v-text-field
-                      v-model="username"
-                      label="Username"
-                      required
+                        v-model="username"
+                        label="Username"
+                        required
                     ></v-text-field>
                     <v-btn color="primary" block type="submit">Login</v-btn>
-                  </v-form>
+                    </v-form>
                 </v-card-text>
-              </v-card>
+                </v-card>
             </v-col>
-          </v-row>
+            </v-row>
         </v-container>
-      </v-main>
+        </v-main>
     </v-app>
-  </template>
-  
-  <script>
-  import { ref } from 'vue';
-  
-  export default {
-    data() {
-      return {
-        email: '',
-        username: ''
-      };
-    },
-    setup() {
-      const login = () => {
-        // ログインの処理を記述する
-        // 例えば、APIリクエストを送信してバックエンドとの認証を行うなど
-      };
-  
-      return {
-        login
-      };
-    }
-  }
-  </script>
-  
+</template>
+<script>
+import { ref } from 'vue';
+export default {
+data() {
+    return {
+    email: '',
+    username: ''
+    };
+},
+setup() {
+    const login = () => {
+    // ログインの処理を記述する
+    // 例えば、APIリクエストを送信してバックエンドとの認証を行うなど
+    };
+    return {
+    login
+    };
+}
+}
+</script>
