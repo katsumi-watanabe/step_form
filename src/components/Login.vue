ff1eb608774344c43bd3ab9242b232d1d01b04dc
渡邊克己 <ktm981124@gmail.com>
2023-07-04 23:04:23 +0900

    ログイン画面変更

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,34 +1,66 @@
 <template>
-    <v-app>
-        <v-main>
-        <v-container fluid class="">
-            <v-row align="center" justify="center">
-            <v-col cols="12" sm="8" md="4">
-                <v-card-title class="text-center">
-                    <h2 class="text--primary">Login</h2>
-                </v-card-title>
-                <v-card-text>
-                    <v-form @submit.prevent="login">
-                    <v-text-field
-                        v-model="email"
-                        label="Email"
-                        type="email"
-                        required
-                    ></v-text-field>
-                    <v-text-field
-                        v-model="username"
-                        label="Username"
-                        required
-                    ></v-text-field>
-                    <v-btn color="primary" block type="submit">Login</v-btn>
-                    </v-form>
-                </v-card-text>
-            </v-col>
-            </v-row>
-        </v-container>
-        </v-main>
-    </v-app>
-</template>
+    <div class="mt-5">
+
+      <v-card
+        class="mx-auto pa-12 pb-8"
+        elevation="8"
+        max-width="448"
+        rounded="lg"
+      >
+        <div class="text-subtitle-1 text-medium-emphasis">Account</div>
+
+        <v-text-field
+          density="compact"
+          placeholder="Email address"
+          prepend-inner-icon="mdi-email-outline"
+          variant="outlined"
+        ></v-text-field>
+
+        <div class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between">
+          Password
+
+          <a
+            class="text-caption text-decoration-none text-blue"
+            href="#"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            Forgot login password?</a>
+        </div>
+
+        <v-text-field
+          :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
+          :type="visible ? 'text' : 'password'"
+          density="compact"
+          placeholder="Enter your password"
+          prepend-inner-icon="mdi-lock-outline"
+          variant="outlined"
+          @click:append-inner="visible = !visible"
+        ></v-text-field>
+
+        <v-btn
+          block
+          class="mb-8"
+          color="blue"
+          size="large"
+          variant="tonal"
+        >
+          Log In
+        </v-btn>
+
+        <v-card-text class="text-center">
+          <a
+            class="text-blue text-decoration-none"
+            href="#"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            Sign up now <v-icon icon="mdi-chevron-right"></v-icon>
+          </a>
+        </v-card-text>
+      </v-card>
+    </div>
+  </template>
 <script>
 import { ref } from 'vue';
 export default {
