7507b13908ab17b84c30056e23b568e0cf6dc3cb
渡邊克己 <ktm981124@gmail.com>
2023-07-08 23:31:01 +0900

    loginしていない場合はloginもしくはsignupページのみ遷移可能

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import Contact2 from '@/components/Contact2.vue'
 import Login from '@/components/Login.vue'
 import Signup from '@/components/Signup.vue'
 import Test from '@/components/Test.vue'
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
 const routes = [
   {
@@ -53,4 +54,22 @@ const router = createRouter({
   routes
 });
 
+// ログイン状態を取得するコード
+const auth = getAuth();
+let isAuthenticated = false;
+
+onAuthStateChanged(auth, (user) => {
+  isAuthenticated = !!user; // ユーザーオブジェクトが存在するかどうかでログイン状態を判断
+});
+
+// ナビゲーションガードを追加
+router.beforeEach((to, from, next) => {
+  // ログイン状態に応じて適切な処理を行う
+
+  if (!isAuthenticated && to.name !== 'Login' && to.name !== 'Signup') {
+    next({ name: 'Login' });
+  } else {
+    next();
+  }
+});
 export default router;
