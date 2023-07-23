ba215cbf9d1b93891d82f10a33affc443ef79efb
渡邊克己 <ktm981124@gmail.com>
2023-07-23 16:13:45 +0900

    エンターでsubmit

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,75 +1,78 @@
 <template>
   <div class="mt-5">
-
     <v-card
       class="mx-auto pa-12 pb-8"
       elevation="8"
       max-width="448"
       rounded="lg"
     >
-      <div class="text-subtitle-1 text-medium-emphasis">アカウント</div>
+      <form @submit.prevent="login">
+        <div class="text-subtitle-1 text-medium-emphasis">アカウント</div>
 
-      <v-text-field
-        density="compact"
-        placeholder="メールアドレス"
-        prepend-inner-icon="mdi-email-outline"
-        variant="outlined"
-        v-model="email"
-      ></v-text-field>
+        <v-text-field
+          density="compact"
+          placeholder="メールアドレス"
+          prepend-inner-icon="mdi-email-outline"
+          variant="outlined"
+          v-model="email"
+        ></v-text-field>
 
-      <div class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between">
-        パスワード
+        <div class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between">
+          パスワード
 
-        <a
-          class="text-caption text-decoration-none template_color_text"
-          href="#"
-          rel="noopener noreferrer"
-          target="_blank"
+          <a
+            class="text-caption text-decoration-none template_color_text"
+            href="#"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            ログインパスワードをお忘れですか？
+          </a>
+        </div>
+
+        <v-text-field
+          :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
+          :type="visible ? 'text' : 'password'"
+          density="compact"
+          placeholder="パスワードを入力してください"
+          prepend-inner-icon="mdi-lock-outline"
+          variant="outlined"
+          v-model="password"
+          @click:append-inner="visible = !visible"
+        ></v-text-field>
+
+        <v-alert
+          v-if="errorMessage"
+          dense
+          outlined
+          type="error"
+          class="mb-4"
         >
-          ログインパスワードをお忘れですか？</a>
-      </div>
+          {{ errorMessage }}
+        </v-alert>
 
-      <v-text-field
-        :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
-        :type="visible ? 'text' : 'password'"
-        density="compact"
-        placeholder="パスワードを入力してください"
-        prepend-inner-icon="mdi-lock-outline"
-        variant="outlined"
-        v-model="password"
-        @click:append-inner="visible = !visible"
-      ></v-text-field>
-
-      <v-alert
-        v-if="errorMessage"
-        dense
-        outlined
-        type="error"
-        class="mb-4"
-      >
-      {{ errorMessage }}
-      </v-alert>
-      <v-btn
-        block
-        class="mb-8 template_color"
-        color="blue"
-        size="large"
-        variant="tonal"
-        @click="login"
-      >
-        ログイン
-      </v-btn>
-
-      <v-card-text class="text-center">
-        <a
-          class="template_color_text text-decoration-none"
-          href="/signup"
-          rel="noopener noreferrer"
-          target="_blank"
+        <v-btn
+          block
+          class="mb-8 template_color"
+          color="blue"
+          size="large"
+          variant="tonal"
+          type="submit"
         >
-          今すぐサインアップ <v-icon icon="mdi-chevron-right"></v-icon>
-        </a>
-      </v-card-text>
+          ログイン
+        </v-btn>
+
+        <v-card-text class="text-center">
+          <a
+            class="template_color_text text-decoration-none"
+            href="/signup"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            今すぐサインアップ <v-icon icon="mdi-chevron-right"></v-icon>
+          </a>
+        </v-card-text>
+      </form>
     </v-card>
   </div>
 </template>
