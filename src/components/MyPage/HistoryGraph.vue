9ecfa386d34aef2ada7a02d7c81834ace6a2dfdb
渡邊克己 <ktm981124@gmail.com>
2023-09-16 22:42:01 +0900

    グラフ調整

--- src/components/MyPage/HistoryGraph.vue
+++ src/components/MyPage/HistoryGraph.vue
@@ -34,27 +34,14 @@ export default {
     return {
       chartData: {
         labels: [
-          'Eating',
-          'Drinking',
-          'Sleeping',
-          'Designing',
-          'Coding',
-          'Cycling',
-          'Running'
+          'Aタイプ',
+          'Bタイプ',
+          'Cタイプ',
+          'Dタイプ'
         ],
         datasets: [{
-          label: 'My First Dataset',
-          data: [65, 59, 90, 81, 56, 55, 40],
-          fill: true,
-          backgroundColor: 'rgba(255, 99, 132, 0.2)',
-          borderColor: 'rgb(255, 99, 132)',
-          pointBackgroundColor: 'rgb(255, 99, 132)',
-          pointBorderColor: '#fff',
-          pointHoverBackgroundColor: '#fff',
-          pointHoverBorderColor: 'rgb(255, 99, 132)'
-        }, {
           label: 'My Second Dataset',
-          data: [28, 48, 40, 19, 96, 27, 100],
+          data: [19, 96, 27, 100],
           fill: true,
           backgroundColor: 'rgba(54, 162, 235, 0.2)',
           borderColor: 'rgb(54, 162, 235)',
@@ -71,3 +58,11 @@ export default {
   }
 }
 </script>
+
+<style>
+#my-chart-id {
+  max-width: 500px;
+  max-height: 500px;
+  margin: 0 auto;
+}
+</style>
