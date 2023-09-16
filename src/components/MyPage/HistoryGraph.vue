d0d0ab501554b38cc0778054aff7c4bd3812904d
渡邊克己 <ktm981124@gmail.com>
2023-09-16 23:12:14 +0900

    wip:一覧

--- src/components/MyPage/HistoryGraph.vue
+++ src/components/MyPage/HistoryGraph.vue
@@ -1,4 +1,9 @@
 <template>
+  <div class="answer-list">
+      <ul>
+        <li v-for="(answer, index) in answers" :key="index">{{ answer }}</li>
+      </ul>
+  </div>
   <Radar
     id="my-chart-id"
     :options="chartOptions"
@@ -17,6 +22,15 @@ import {
   Legend
 } from 'chart.js'
 import { Radar } from 'vue-chartjs'
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+};
+
+firebase.initializeApp(firebaseConfig);
+console.log(firebase.initializeApp(firebaseConfig));
 
 ChartJS.register(
   RadialLinearScale,
@@ -53,9 +67,24 @@ export default {
       },
       chartOptions: {
         responsive: true
-      }
-    }
-  }
+      },
+      answers: []
+    };
+  },
+  created() {
+    // Firestoreからデータを取得してanswersに設定
+    const db = firebase.firestore();
+    db.collection('answers')
+      .get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          this.answers.push(doc.data().answer);
+        });
+      })
+      .catch((error) => {
+        console.error('Error getting documents: ', error);
+      });
+  },
 }
 </script>
 
@@ -64,5 +93,19 @@ export default {
   max-width: 500px;
   max-height: 500px;
   margin: 0 auto;
+}
+
+.answer-list {
+  margin-top: 20px;
+}
+
+.answer-list ul {
+  list-style: none;
+  padding: 0;
+}
+
+.answer-list li {
+  margin-bottom: 10px;
+  font-size: 16px;
 }
 </style>
